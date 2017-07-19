fbb684503def520c8ddb542f372be27e92585acd
yiwankaixuan <yiwankaixuan@gmail.com>
2017-07-19 03:08:06 +0000

    Procfile for Heroku

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Fucking guys！</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="http://weibo.com/lanjiangyue?is_all=1">伊万凯旋PHP自学之路</a>主页。
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">使劲戳</a>
+    </p>
+  </div>
 @stop
